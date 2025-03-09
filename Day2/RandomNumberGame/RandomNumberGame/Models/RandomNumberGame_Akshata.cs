c58deedae4c11f9812501db972515b266afd62d7
Akshata Ubale <akshata2202.01@gmail.com>
2025-03-09 22:39:32 +0530

    Day 2 : Added logic of playing Random Number Game in RandomNumberGame_Akshata class

--- Day2/RandomNumberGame/RandomNumberGame/Models/RandomNumberGame_Akshata.cs
+++ Day2/RandomNumberGame/RandomNumberGame/Models/RandomNumberGame_Akshata.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,46 @@ namespace RandomNumberGame.Models
 {
     internal class RandomNumberGame_Akshata
     {
+        int win = 0, lose = 0;
+        public RandomNumberGame_Akshata() { }
+
+        /// <summary>
+        /// Play the Game
+        /// </summary>
+        public void Execute() 
+        {
+            PlayGame();
+        }
+
+        /// <summary>
+        /// Generate random number
+        /// </summary>
+        /// <returns>return a random number</returns>
+        public int GenerateRandomNumber()
+        {
+            Random random = new Random();
+            return random.Next(11);
+        }
+
+        /// <summary>
+        /// Check the random number 3 times and decide if player wins or loses
+        /// </summary>
+        public void PlayGame()
+        {
+            Console.WriteLine("Generated Random Number");
+            for (int i = 0; i < 3; i++)
+            {
+                int randomNum = GenerateRandomNumber();
+                Console.Write(randomNum+"   ");
+                if (randomNum % 2 == 0) 
+                    win++;
+                else 
+                    lose++;
+            }
+            if (win > lose)
+                Console.WriteLine("\nHurray!! You Win the Game");
+            else
+                Console.WriteLine("\nOops!! You Lost the Game");
+        }
     }
 }
--- Day2/RandomNumberGame/RandomNumberGame/Program.cs
+++ Day2/RandomNumberGame/RandomNumberGame/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using RandomNumberGame.Models;
+
 Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("--------------------------------------------");
+
+RandomNumberGame_Akshata randomNumGameAk = new RandomNumberGame_Akshata();
+randomNumGameAk.Execute();
+
+Console.WriteLine("--------------------------------------------");
