3c7c46663478b99f0b99c6ea981bb0f4fc87e177
Vaibhav <vaibhavambhore737@gmail.com>
2025-03-10 22:27:40 +0530

    Day 2 : Added code to check Palindrome

--- Day3/CheckPalindrome/CheckPalindrome/Models/CheckPalindrome_Vaibhav.cs
+++ Day3/CheckPalindrome/CheckPalindrome/Models/CheckPalindrome_Vaibhav.cs
@@ -8,5 +8,14 @@ namespace CheckPalindrome.Models
 {
     internal class CheckPalindrome_Vaibhav
     {
+        public string IsPalindrome(string s)
+        {
+            for (int i=0; i<s.Length/2;i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                    return "It is not palindrome";
+            }
+            return "It is palindrome";
+        }
     }
 }
--- Day3/CheckPalindrome/CheckPalindrome/Program.cs
+++ Day3/CheckPalindrome/CheckPalindrome/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace CheckPalindrome
+﻿using CheckPalindrome.Models;
+
+namespace CheckPalindrome
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CheckPalindrome_Vaibhav vda = new CheckPalindrome_Vaibhav();
+            Console.WriteLine(vda.IsPalindrome("abcddcba"));
+            Console.WriteLine(vda.IsPalindrome("abcdcda"));
         }
     }
 }
