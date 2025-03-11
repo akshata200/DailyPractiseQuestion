915ddf942c56e2306736cf888cab29caaea1f9d8
Akshata Ubale <akshata2202.01@gmail.com>
2025-03-11 23:26:52 +0530

    Day 4 : Added Logic for Absolute Number

--- Day4/AbsoluteNumber/AbsoluteNumber/Models/AbsoluteNumValue_Akshata.cs
+++ Day4/AbsoluteNumber/AbsoluteNumber/Models/AbsoluteNumValue_Akshata.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,46 @@ namespace AbsoluteNumber.Models
 {
     internal class AbsoluteNumValue_Akshata
     {
+        int inputNum;
+
+        public AbsoluteNumValue_Akshata()
+        {
+            this.inputNum = 0;
+        }
+
+        /// <summary>
+        /// Executor code for taking input and printing Absolute Value
+        /// </summary>
+        public void Executor()
+        {
+            TakeInput();
+        }
+
+        /// <summary>
+        /// Input Integer from console
+        /// </summary>
+        public void TakeInput()
+        {
+            Console.WriteLine("Input a number");
+            try
+            {
+                this.inputNum = int.Parse(Console.ReadLine());
+                PrintAbsoluteValue();
+            }
+            catch (Exception e) 
+            { 
+                Console.WriteLine("Invalid input : "+e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Print Absolute value of input integer
+        /// </summary>
+        public void PrintAbsoluteValue()
+        {
+            if (this.inputNum < 0)
+                this.inputNum = this.inputNum * -1;
+            Console.WriteLine($"Absolute value is {this.inputNum}");
+        }
     }
 }
--- Day4/AbsoluteNumber/AbsoluteNumber/Program.cs
+++ Day4/AbsoluteNumber/AbsoluteNumber/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AbsoluteNumber.Models;
+
 Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("\n----------------------------------------\n");
+AbsoluteNumValue_Akshata AbsNumAk = new AbsoluteNumValue_Akshata();
+AbsNumAk.Executor();
+Console.WriteLine("\n----------------------------------------\n");
